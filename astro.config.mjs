199c414f695a3f6204fed9a3a6ffc6270ce08189
tsar-boomba <itg.2048@gmail.com>
2024-04-23 20:06:05 -0400

    Add header, improve hero vastly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		css: {
-			transformer: 'lightningcss',
-		},
-	},
+  vite: {
+    css: {
+      transformer: 'lightningcss'
+    }
+  },
+  integrations: [icon()]
 });
